a608ed33f02d1b43fc36e0c68cecd438755b3eb7
TimvdVuurst~ <timvdvuurst@gmail.com>
2025-01-04 18:31:00 +0100

    Implemented regression dataset (seemingly working) and task (lightning trainer yields errors).

--- RegressionData.py
+++ RegressionData.py
@@ -1,10 +1,18 @@
 from torchvision.datasets import ImageFolder
 from terratorch.datamodules import GenericNonGeoClassificationDataModule
-from terratorch.datasets import generic_scalar_label_dataset
+from terratorch.datasets.generic_scalar_label_dataset import GenericScalarLabelDataset
+from terratorch.datasets.utils import HLSBands
 import numpy as np
-import pandas as pd
-from typing import Any,Tuple,A,HLSBands,Path
+from typing import Any,Iterable
+from pathlib import Path
+import albumentations as A
 import os
+from torchgeo.datamodules import NonGeoDataModule
+from torch import Tensor
+from torch.utils.data import DataLoader
+
+from terratorch.io.file import load_from_file_or_attribute
+from terratorch.datamodules.generic_scalar_label_data_module import wrap_in_compose_is_list, Normalize
 
 # class ModifiedImageFolder(ImageFolder):
 #     def __getitem__(self, index: int) -> Tuple[Any, Any]:
@@ -24,6 +32,7 @@ import os
 #             target = self.target_transform(target)
 
 #         return sample, target, regression_ground_truth
+
 
 def itemgetter(index,samples,loader,transform,target_transform):
         """
@@ -46,14 +55,53 @@ def itemgetter(index,samples,loader,transform,target_transform):
         return sample, target, regression_ground_truth
 
 
-class GenericClassificationRegressionDataset(generic_scalar_label_dataset):
- def __getitem__(self, index: int) -> dict[str, Any]:
+class GenericClassificationRegressionDataset(GenericScalarLabelDataset):
+    def __init__(
+        self,
+        data_root: Path,
+        num_classes: int,
+        split: Path | None = None,
+        ignore_split_file_extensions: bool = True,  # noqa: FBT001, FBT002
+        allow_substring_split_file: bool = True,  # noqa: FBT001, FBT002
+        rgb_indices: list[str] | None = None,
+        dataset_bands: list[HLSBands | int] | None = None,
+        output_bands: list[HLSBands | int] | None = None,
+        class_names: list[str] | None = None,
+        constant_scale: float = 1,
+        transform: A.Compose | None = None,
+        no_data_replace: float = 0,
+        expand_temporal_dimension: bool = False,  # noqa: FBT001, FBT002
+    ) -> None:
+        """
+        Based heavily on TerraTorch's GenericNonGeoClassificationDataset. See the source code there for more information.
+        """
+        super().__init__(
+            data_root,
+            split=split,
+            ignore_split_file_extensions=ignore_split_file_extensions,
+            allow_substring_split_file=allow_substring_split_file,
+            rgb_indices=rgb_indices,
+            dataset_bands=dataset_bands,
+            output_bands=output_bands,
+            constant_scale=constant_scale,
+            transform=transform,
+            no_data_replace=no_data_replace,
+            expand_temporal_dimension=expand_temporal_dimension,
+        )
+        self.num_classes = num_classes
+        self.class_names = class_names
+
+    def __getitem__(self, index: int) -> dict[str, Any]:
+        """ 
+        Altered version of the __getitem__ function of GenericScalarLabelDataset.
+        """
+        ##Ignore these two lines, kept only for reference
         #not sure if this will work since generic_scalar_label_dataset inherits from ImageFolder, but ay if it calls it calls
         # image, label,regression_ground_truth = ModifiedImageFolder.__getitem__(self, index) ##changed to ModifiedImageFolder
 
-        ## Should work, not the best way to do it but ay 
+        ## Should work, not the best way to do it but it works 
         image, label,regression_ground_truth = itemgetter(index,self.samples,self.loader,self.transform,self.target_transform) 
-       ## Commented out since idk where the rearrange is defined
+        ## Commented out since idk where the rearrange is defined
         # if self.expand_temporal_dimension:
         #     image = rearrange(image, "h w (channels time) -> time h w channels", channels=len(self.output_bands))
         if self.filter_indices:
@@ -67,15 +115,19 @@ class GenericClassificationRegressionDataset(generic_scalar_label_dataset):
         output = {
             "image": image,
             "label": label,
-            'regr_label': regression_ground_truth, #ADDED
+            'regr_label': regression_ground_truth, #ADDED for regression
             "filename": self.image_files[index]
         }
 
         return output
+    
+
+    def plot(self, sample, suptitle = None):
+        pass
  
 
- class GenericNonGeoClassificationRegressionDataModule(GenericNonGeoClassificationDataModule):
-        def __init__(
+class GenericNonGeoClassificationRegressionDataModule(NonGeoDataModule):
+    def __init__(
         self,
         batch_size: int,
         num_workers: int,
@@ -102,34 +154,120 @@ class GenericClassificationRegressionDataset(generic_scalar_label_dataset):
         expand_temporal_dimension: bool = False,
         no_data_replace: float = 0,
         drop_last: bool = True,
-        **kwargs: Any) -> None:
-            super().__init__( batch_size,
-                            num_workers,
-                            train_data_root,
-                            val_data_root,
-                            test_data_root,
-                            means,
-                            stds,
-                            num_classes,
-                            predict_data_root,
-                            train_split,
-                            val_split,
-                            test_split,
-                            ignore_split_file_extensions,
-                            allow_substring_split_file,
-                            dataset_bands,
-                            predict_dataset_bands,
-                            output_bands,
-                            constant_scale,
-                            rgb_indices,
-                            train_transform,
-                            val_transform, 
-                            test_transform,
-                            expand_temporal_dimension,
-                            no_data_replace,
-                            drop_last,
-                            **kwargs)
-            #this is really the only difference here, change it to the dataset defined above that is able to extract regression data from
-            #the filenamebut since this variable is defined in a class that GenericNonGeoClassificationDataModule inherits from and this
-            #felt like the easiest way to do it
-            self.dataset_class = GenericClassificationRegressionDataset 
+        **kwargs: Any,
+    ) -> None:
+        """ 
+        Heavily based on Terratorch's GenericNonGeoClassificationDataModule. See the source code there for more information.
+        """
+        super().__init__(GenericClassificationRegressionDataset, batch_size, num_workers, **kwargs) #giving the right dataset class 
+        self.num_classes = num_classes
+        self.train_root = train_data_root
+        self.val_root = val_data_root
+        self.test_root = test_data_root
+        self.predict_root = predict_data_root
+        self.train_split = train_split
+        self.val_split = val_split
+        self.test_split = test_split
+        self.ignore_split_file_extensions = ignore_split_file_extensions
+        self.allow_substring_split_file = allow_substring_split_file
+        self.constant_scale = constant_scale
+        self.no_data_replace = no_data_replace
+        self.drop_last = drop_last
+
+        self.dataset_bands = dataset_bands
+        self.predict_dataset_bands = predict_dataset_bands if predict_dataset_bands else dataset_bands
+        self.output_bands = output_bands
+        self.rgb_indices = rgb_indices
+        self.expand_temporal_dimension = expand_temporal_dimension
+
+        self.train_transform = wrap_in_compose_is_list(train_transform)
+        self.val_transform = wrap_in_compose_is_list(val_transform)
+        self.test_transform = wrap_in_compose_is_list(test_transform)
+
+        means = load_from_file_or_attribute(means)
+        stds = load_from_file_or_attribute(stds)
+
+        self.aug = Normalize(means, stds)
+
+    def setup(self, stage: str) -> None:
+        if stage in ["fit"]:
+            self.train_dataset = self.dataset_class(
+                self.train_root,
+                self.num_classes,
+                split=self.train_split,
+                ignore_split_file_extensions=self.ignore_split_file_extensions,
+                allow_substring_split_file=self.allow_substring_split_file,
+                dataset_bands=self.dataset_bands,
+                output_bands=self.output_bands,
+                constant_scale=self.constant_scale,
+                rgb_indices=self.rgb_indices,
+                transform=self.train_transform,
+                no_data_replace=self.no_data_replace,
+                expand_temporal_dimension=self.expand_temporal_dimension,
+            )
+        if stage in ["fit", "validate"]:
+            self.val_dataset = self.dataset_class(
+                self.val_root,
+                self.num_classes,
+                split=self.val_split,
+                ignore_split_file_extensions=self.ignore_split_file_extensions,
+                allow_substring_split_file=self.allow_substring_split_file,
+                dataset_bands=self.dataset_bands,
+                output_bands=self.output_bands,
+                constant_scale=self.constant_scale,
+                rgb_indices=self.rgb_indices,
+                transform=self.val_transform,
+                no_data_replace=self.no_data_replace,
+                expand_temporal_dimension=self.expand_temporal_dimension,
+            )
+        if stage in ["test"]:
+            self.test_dataset = self.dataset_class(
+                self.test_root,
+                self.num_classes,
+                split=self.test_split,
+                ignore_split_file_extensions=self.ignore_split_file_extensions,
+                allow_substring_split_file=self.allow_substring_split_file,
+                dataset_bands=self.dataset_bands,
+                output_bands=self.output_bands,
+                constant_scale=self.constant_scale,
+                rgb_indices=self.rgb_indices,
+                transform=self.test_transform,
+                no_data_replace=self.no_data_replace,
+                expand_temporal_dimension=self.expand_temporal_dimension,
+            )
+        if stage in ["predict"] and self.predict_root:
+            self.predict_dataset = self.dataset_class(
+                self.predict_root,
+                self.num_classes,
+                dataset_bands=self.predict_dataset_bands,
+                output_bands=self.output_bands,
+                constant_scale=self.constant_scale,
+                rgb_indices=self.rgb_indices,
+                transform=self.test_transform,
+                no_data_replace=self.no_data_replace,
+                expand_temporal_dimension=self.expand_temporal_dimension,
+            )
+
+    def _dataloader_factory(self, split: str) -> DataLoader[dict[str, Tensor]]:
+        """Implement one or more PyTorch DataLoaders.
+
+        Args:
+            split: Either 'train', 'val', 'test', or 'predict'.
+
+        Returns:
+            A collection of data loaders specifying samples.
+
+        Raises:
+            MisconfigurationException: If :meth:`setup` does not define a
+                dataset or sampler, or if the dataset or sampler has length 0.
+        """
+        dataset = self._valid_attribute(f"{split}_dataset", "dataset")
+        batch_size = self._valid_attribute(f"{split}_batch_size", "batch_size")
+        return DataLoader(
+            dataset=dataset,
+            batch_size=batch_size,
+            shuffle=split == "train",
+            num_workers=self.num_workers,
+            collate_fn=self.collate_fn,
+            drop_last=split == "train" and self.drop_last,
+        )
--- regression_tasks.py
+++ regression_tasks.py
@@ -1,5 +1,5 @@
 from typing import Any
-import logging
+
 import lightning
 import matplotlib.pyplot as plt
 import torch
@@ -7,48 +7,34 @@ from lightning.pytorch.callbacks import Callback
 from segmentation_models_pytorch.losses import FocalLoss, JaccardLoss
 from torch import Tensor, nn
 from torchgeo.datasets.utils import unbind_samples
+from torchgeo.trainers import BaseTask
 from torchmetrics import ClasswiseWrapper, MetricCollection
-from torchmetrics.classification import MulticlassAccuracy, MulticlassFBetaScore, MulticlassJaccardIndex
-from torchmetrics.regression import R2Score
+# from torchmetrics.classification import MulticlassAccuracy, MulticlassFBetaScore, MulticlassJaccardIndex
+from torchmetrics.regression import R2Score, SpearmanCorrCoef
 
-from terratorch.models.model import AuxiliaryHead, Model, ModelOutput
-# from terratorch.registry.registry import MODEL_FACTORY_REGISTRY
-from terratorch_registry import MODEL_FACTORY_REGISTRY
+
+from terratorch.models.model import AuxiliaryHead, Model, ModelOutput, get_factory
 from terratorch.tasks.loss_handler import LossHandler
 from terratorch.tasks.optimizer_factory import optimizer_factory
-# from terratorch.tasks.base_task import TerraTorchTask
-from terratorch_basetask import TerraTorchTask
 
-logger = logging.getLogger('terratorch')
 
 def to_class_prediction(y: ModelOutput) -> Tensor:
     y_hat = y.output
     return y_hat.argmax(dim=1)
 
 
-class RegressionTask(TerraTorchTask):  # changed from ClassificationTask
-    """Classification Task that accepts models from a range of sources.
+class RegressionTask(BaseTask):  # changed from ClassificationTask
+    """Regression Task that accepts models from a range of sources.
 
-    This class is analog in functionality to class:ClassificationTask defined by torchgeo.
-    However, it has some important differences:
-        - Accepts the specification of a model factory
-        - Logs metrics per class
-        - Does not have any callbacks by default (TorchGeo tasks do early stopping by default)
-        - Allows the setting of optimizers in the constructor
-        - It provides mIoU with both Micro and Macro averaging
-
-    .. note::
-           * 'Micro' averaging suits overall performance evaluation but may not reflect
-             minority class accuracy.
-           * 'Macro' averaging gives equal weight to each class, useful
-             for balanced performance assessment across imbalanced classes.
+    This class is heavily based on the ClassificationTask from Terratorch. See
+    the documentation there for more details. Most code credit goes to that class,
+    where changes are made these are noted explicitly.
     """
 
     def __init__(
         self,
         model_args: dict,
-        model_factory: str | None = None,
-        model: torch.nn.Module | None = None,
+        model_factory: str,
         loss: str = "ce",
         aux_heads: list[AuxiliaryHead] | None = None,
         aux_loss: dict[str, float] | None = None,
@@ -66,15 +52,15 @@ class RegressionTask(TerraTorchTask):  # changed from ClassificationTask
         freeze_decoder: bool = False,  # noqa: FBT002, FBT001
         class_names: list[str] | None = None,
     ) -> None:
+        
+        
         """Constructor
 
         Args:
 
             Defaults to None.
             model_args (Dict): Arguments passed to the model factory.
-            model_factory (str, optional): ModelFactory class to be used to instantiate the model.
-                Is ignored when model is provided.
-            model (torch.nn.Module, optional): Custom model.
+            model_factory (str): ModelFactory class to be used to instantiate the model.
             loss (str, optional): Loss to be used. Currently, supports 'ce', 'jaccard' or 'focal' loss.
                 Defaults to "ce".
             aux_loss (dict[str, float] | None, optional): Auxiliary loss weights.
@@ -103,26 +89,41 @@ class RegressionTask(TerraTorchTask):  # changed from ClassificationTask
         """
         self.aux_loss = aux_loss
         self.aux_heads = aux_heads
-
-        if model is not None and model_factory is not None:
-            logger.warning("A model_factory and a model was provided. The model_factory is ignored.")
-        if model is None and model_factory is None:
-            raise ValueError("A model_factory or a model (torch.nn.Module) must be provided.")
-
-        if model_factory and model is None:
-            self.model_factory = MODEL_FACTORY_REGISTRY.build(model_factory)
-
-        super().__init__(task="classification")
-
-        if model:
-            # Custom model
-            self.model = model
-
+        self.model_factory = get_factory(model_factory)
+        super().__init__()
         self.train_loss_handler = LossHandler(self.train_metrics.prefix)
         self.test_loss_handler = LossHandler(self.test_metrics.prefix)
         self.val_loss_handler = LossHandler(self.val_metrics.prefix)
         self.monitor = f"{self.val_metrics.prefix}loss"
 
+    # overwrite early stopping
+    def configure_callbacks(self) -> list[Callback]:
+        return []
+
+    def configure_models(self) -> None:
+        self.model: Model = self.model_factory.build_model(
+            "classification", aux_decoders=self.aux_heads, **self.hparams["model_args"] #if you change classification to regression it will try to build a pixelwise regression model
+        )
+        if self.hparams["freeze_backbone"]:
+            self.model.freeze_encoder()
+        if self.hparams["freeze_decoder"]:
+            self.model.freeze_decoder()
+
+    def configure_optimizers(
+        self,
+    ) -> "lightning.pytorch.utilities.types.OptimizerLRSchedulerConfig":
+        optimizer = self.hparams["optimizer"]
+        if optimizer is None:
+            optimizer = "Adam"
+        return optimizer_factory(
+            self.hparams["optimizer"],
+            self.hparams["lr"],
+            self.parameters(),
+            self.hparams["optimizer_hparams"],
+            self.hparams["scheduler"],
+            self.monitor,
+            self.hparams["scheduler_hparams"],
+        )
 
     def configure_losses(self) -> None:
         """Initialize the loss criterion.
@@ -136,21 +137,13 @@ class RegressionTask(TerraTorchTask):  # changed from ClassificationTask
         class_weights = (
                     torch.Tensor(self.hparams["class_weights"]) if self.hparams["class_weights"] is not None else None
                 )
-        if loss == "ce":
+        if loss == "mse":  # added to handle regression, for now no other loss is valid
             ignore_value = -100 if ignore_index is None else ignore_index
-            self.criterion = nn.CrossEntropyLoss(ignore_index=ignore_value, weight=class_weights)
-        elif loss == "bce":
-            self.criterion = nn.BCEWithLogitsLoss()
-        elif loss == "jaccard":
-            self.criterion = JaccardLoss(mode="multiclass")
-        elif loss == "focal":
-            self.criterion = FocalLoss(mode="multiclass", normalized=True)
-        elif loss == "mse":  # added to handle regression
-            ignore_value = -100 if ignore_index is None else ignore_index
-            self.criterion = nn.MSELoss(ignore_index=ignore_value, weight=class_weights)  
+            self.criterion = nn.MSELoss  
         else:
             msg = f"Loss type '{loss}' is not valid."
             raise ValueError(msg)
+
 
     def configure_metrics(self) -> None:
         """Initialize the performance metrics."""
@@ -158,48 +151,17 @@ class RegressionTask(TerraTorchTask):  # changed from ClassificationTask
         ignore_index: int = self.hparams["ignore_index"]
         class_names = self.hparams["class_names"]
         metrics = MetricCollection(
-            {
-                "Overall_Accuracy": MulticlassAccuracy(
-                    num_classes=num_classes,
-                    ignore_index=ignore_index,
-                    average="micro",
-                ),
-                "Average_Accuracy": MulticlassAccuracy(
-                    num_classes=num_classes,
-                    ignore_index=ignore_index,
-                    average="macro",
-                ),
-                "Multiclass_Accuracy_Class": ClasswiseWrapper(
-                    MulticlassAccuracy(
-                        num_classes=num_classes,
-                        ignore_index=ignore_index,
-                        average=None,
-                    ),
-                    labels=class_names,
-                ),
-                "Multiclass_Jaccard_Index": MulticlassJaccardIndex(num_classes=num_classes, ignore_index=ignore_index),
-                "Multiclass_Jaccard_Index_Class": ClasswiseWrapper(
-                    MulticlassJaccardIndex(num_classes=num_classes, ignore_index=ignore_index, average=None),
-                    labels=class_names,
-                ),
-                # why FBetaScore
-                "Multiclass_F1_Score": MulticlassFBetaScore(
-                    num_classes=num_classes,
-                    ignore_index=ignore_index,
-                    beta=1.0,
-                    average="micro",
-                ),
-                # R2 for regression
+            {            
                 "R2_Score": R2Score(
                     num_outputs=num_classes
-                )
+                ),
+                'Spearman Correlation': SpearmanCorrCoef(num_outputs=num_classes)
             }
         )
         self.train_metrics = metrics.clone(prefix="train/")
         self.val_metrics = metrics.clone(prefix="val/")
         self.test_metrics = metrics.clone(prefix="test/")
 
-    ##CHANGES MADE IN THIS FUNCTION SHOULD BE MADE ANALOGOUSLY IN validation_step AND test_step
     def training_step(self, batch: Any, batch_idx: int, dataloader_idx: int = 0) -> Tensor:
         """Compute the train loss and additional metrics.
 
@@ -209,17 +171,19 @@ class RegressionTask(TerraTorchTask):  # changed from ClassificationTask
             dataloader_idx: Index of the current dataloader.
         """
         x = batch["image"]
-        y = batch["label"] #CHANGE TO REGRESSION GROUND TRUTH AS PER RegressionData.py (e.g. use the key regr_label)
-        other_keys = batch.keys() - {"image", "label", "filename"} #ADD IN THE REGRESSION GROUND TRUTH
-        rest = {k:batch[k] for k in other_keys}
-
-        model_output: ModelOutput = self(x, **rest)
+        y = batch["regr_label"] #CHANGE TO REGRESSION GROUND TRUTH AS PER RegressionData.py (e.g. use the key regr_label)
+        model_output: dict[str, Tensor] = self(x)
         loss = self.train_loss_handler.compute_loss(model_output, y, self.criterion, self.aux_loss)
         self.train_loss_handler.log_loss(self.log, loss_dict=loss, batch_size=x.shape[0])
-        y_hat_hard = to_class_prediction(model_output) ##CHANGE TO REGRESSION PREDICTOR, SAME IN VAL AND TEST(?)
+        y_hat_hard = to_class_prediction(model_output)
         self.train_metrics.update(y_hat_hard, y)
 
         return loss["loss"]
+   
+    def on_train_epoch_end(self) -> None:
+        self.log_dict(self.train_metrics.compute(), sync_dist=True)
+        self.train_metrics.reset()
+        return super().on_train_epoch_end()
 
     def validation_step(self, batch: Any, batch_idx: int, dataloader_idx: int = 0) -> None:
         """Compute the validation loss and additional metrics.
@@ -230,14 +194,17 @@ class RegressionTask(TerraTorchTask):  # changed from ClassificationTask
             dataloader_idx: Index of the current dataloader.
         """
         x = batch["image"]
-        y = batch["label"]
-        other_keys = batch.keys() - {"image", "label", "filename"}
-        rest = {k:batch[k] for k in other_keys}
-        model_output: ModelOutput = self(x, **rest)
+        y = batch["regr_label"] #CHANGE TO REGRESSION GROUND TRUTH AS PER RegressionData.py (e.g. use the key regr_label)
+        model_output: dict[str, Tensor] = self(x)
         loss = self.val_loss_handler.compute_loss(model_output, y, self.criterion, self.aux_loss)
         self.val_loss_handler.log_loss(self.log, loss_dict=loss, batch_size=x.shape[0])
         y_hat_hard = to_class_prediction(model_output)
         self.val_metrics.update(y_hat_hard, y)
+
+    def on_validation_epoch_end(self) -> None:
+        self.log_dict(self.val_metrics.compute(), sync_dist=True)
+        self.val_metrics.reset()
+        return super().on_validation_epoch_end()
 
     def test_step(self, batch: Any, batch_idx: int, dataloader_idx: int = 0) -> None:
         """Compute the test loss and additional metrics.
@@ -248,14 +215,17 @@ class RegressionTask(TerraTorchTask):  # changed from ClassificationTask
             dataloader_idx: Index of the current dataloader.
         """
         x = batch["image"]
-        y = batch["label"]
-        other_keys = batch.keys() - {"image", "label", "filename"} 
-        rest = {k:batch[k] for k in other_keys}
-        model_output: ModelOutput = self(x, **rest)
+        y = batch["regr_label"] #CHANGE TO REGRESSION GROUND TRUTH AS PER RegressionData.py (e.g. use the key regr_label)
+        model_output: dict[str, Tensor] = self(x)
         loss = self.test_loss_handler.compute_loss(model_output, y, self.criterion, self.aux_loss)
         self.test_loss_handler.log_loss(self.log, loss_dict=loss, batch_size=x.shape[0])
         y_hat_hard = to_class_prediction(model_output)
         self.test_metrics.update(y_hat_hard, y)
+
+    def on_test_epoch_end(self) -> None:
+        self.log_dict(self.test_metrics.compute(), sync_dist=True)
+        self.test_metrics.reset()
+        return super().on_test_epoch_end()
 
     def predict_step(self, batch: Any, batch_idx: int, dataloader_idx: int = 0) -> Tensor:
         """Compute the predicted class probabilities.
@@ -270,9 +240,6 @@ class RegressionTask(TerraTorchTask):  # changed from ClassificationTask
         """
         x = batch["image"]
         file_names = batch["filename"]
-        other_keys = batch.keys() - {"image", "label", "filename"}
-        rest = {k:batch[k] for k in other_keys}
-        model_output: ModelOutput = self(x, **rest)
 
         y_hat = self(x).output
         y_hat = y_hat.argmax(dim=1)
